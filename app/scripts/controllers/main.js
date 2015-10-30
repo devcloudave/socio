8ecd40566eb9eabc7a365330afffafa395da6426
devcloudave <devcloudave@gmail.com>
2015-10-29 22:18:52 -0300

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name socioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the socioApp
+ */
+angular.module('socioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
